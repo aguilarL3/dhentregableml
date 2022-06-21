4ce29ed3669bf2913610e348d206152d2f63b86b
aguilarL3 <aguilarleandro33@gmail.com>
2022-06-20 22:30:48 -0300

    primer proyecto

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express")
+
+const path = require("path")
+
+const app = express()
+
+// creado recursos de archivos estaticos
+const publicPath = path.resolve(__dirname, "./public");
+
+// middelware
+app.use(express.static(publicPath))
+
+
+//rutas : get, post, put, delete
+app.listen(3001,()=>{
+    console.log("servidor escuchando puerto 3001 funcionando")
+})
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/home.html"))
+})
+
+app.get("/produc",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/form_produc.html"))
+})
+
+app.get("/register",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/register.html"))
+})
+
+app.get("/login",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/login.html"))
+})
